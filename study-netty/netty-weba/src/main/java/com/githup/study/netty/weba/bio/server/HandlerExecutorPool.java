2dfe9cbd6b9c0e87570fdf81eca81ea9cf11e346
longhairen <longhairen@lvmama.com>
2017-12-05 10:05:02 +0800

    study

---
+++ study-netty/netty-weba/src/main/java/com/githup/study/netty/weba/bio/server/HandlerExecutorPool.java
@@ -0,0 +1,24 @@
+package com.githup.study.netty.weba.bio.server;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+
+/**
+ * @author longhr
+ * @version 2017/12/4 0004
+ */
+public class HandlerExecutorPool {
+    private ExecutorService executor;
+
+    public HandlerExecutorPool(int maxSize, int queueSize) {
+        this.executor = new ThreadPoolExecutor(Runtime.getRuntime().availableProcessors(), maxSize, 120L,
+                TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(Runtime.getRuntime().availableProcessors()));
+    }
+
+    public void execute(Runnable task) {
+        executor.execute(task);
+    }
+}
