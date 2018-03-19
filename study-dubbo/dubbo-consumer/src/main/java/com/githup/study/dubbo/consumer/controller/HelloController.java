6fa7fdf3e48ef710c9b49fd74f3274f55fb804b0
longhairen@lvmama.com <longhairen@lvmama.com>
2018-03-19 11:37:33 +0800

    study

--- study-dubbo/dubbo-consumer/src/main/java/com/githup/study/dubbo/consumer/controller/HelloController.java
+++ study-dubbo/dubbo-consumer/src/main/java/com/githup/study/dubbo/consumer/controller/HelloController.java
@@ -1,10 +1,5 @@
 package com.githup.study.dubbo.consumer.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.lvmama.tnt.common.domain.PageResponseDTO;
-import com.lvmama.tnt.export.code.api.domain.vo.ExportProductVO;
-import com.lvmama.tnt.export.code.api.service.ITntCodeGoodsRemoteService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,20 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @Autowired
-    private ITntCodeGoodsRemoteService tntCodeGoodsRemoteService;
 
     @RequestMapping("/hello")
     public String hello() {
         return "hello world";
     }
 
-    @RequestMapping("/invoke")
-    public String invoke() {
-        ExportProductVO vo = new ExportProductVO();
-        vo.setPageSize(10);
-        vo.setPageNo(1);
-        PageResponseDTO responseDTO =  tntCodeGoodsRemoteService.findByPage(vo);
-        return JSON.toJSONString(responseDTO);
-    }
 }
---
+++ study-thread/thread-base/src/main/java/com/github/study/thread/base/group/MasterThread.java
@@ -0,0 +1,32 @@
+package com.github.study.thread.base.group;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ForkJoinPool;
+
+/**
+ *
+ */
+public class MasterThread {
+
+    public static void main(String[] args) throws Exception{
+        int tcount = 5;
+        final CyclicBarrier cyclicBarrier = new CyclicBarrier(tcount);
+
+        for (int i = 0; i < tcount; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        cyclicBarrier.await();
+                        System.out.println(Thread.currentThread().getName());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (BrokenBarrierException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
+    }
+}
