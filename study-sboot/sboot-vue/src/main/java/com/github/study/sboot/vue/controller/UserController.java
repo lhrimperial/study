b7c22a444413ec59f832852cb67e2b8a6872b837
longhairen@lvmama.com <longhairen@lvmama.com>
2018-04-03 19:30:10 +0800

    study

--- study-sboot/sboot-vue/src/main/java/com/github/study/sboot/vue/controller/UserController.java
+++ study-sboot/sboot-vue/src/main/java/com/github/study/sboot/vue/controller/UserController.java
@@ -1,10 +1,8 @@
 package com.github.study.sboot.vue.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.github.study.sboot.vue.domain.User;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,7 +14,6 @@ import javax.servlet.http.HttpServletResponse;
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class UserController {
 
-    @ResponseBody
     @RequestMapping("/user")
     public User getUser(HttpServletRequest request, HttpServletResponse response) {
         String id = request.getParameter("id");
@@ -24,5 +21,11 @@ public class UserController {
         System.out.println("url:"+url);
         System.out.println(id);
         return new User("1", "hello");
+    }
+
+    @RequestMapping("/add")
+    public String addUser(@RequestParam User user) {
+        System.out.println(JSON.toJSONString(user));
+        return "successfully";
     }
 }
